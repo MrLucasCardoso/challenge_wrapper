87851b5184053737dddc326a7a5dffe2203ca7af
Lucas Cardoso <mr.lucascardoso@gmail.com>
2018-06-30 02:23:54 -0300

    Creating setup.py and initializing virtual env

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='challenge_wrapper',
+    packages=['challenge_wrapper'], 
+    version='0.1',
+    description='This library will act as a wrapper around the API for a challenge',
+    author='Lucas Cardoso',
+    author_email='mr.lucascardoso@gmail.com',
+    url='https://github.com/mrlucascardoso/challenge_wrapper',
+    classifiers=[],
+    install_requires=[]
+)
